b0c3d212f213d35af8eb1e368d3d1f11d71b69c6
Amanda <Amanda_moller@estudante.sesisenai.og.br>
2025-08-11 15:18:47 -0300

    Add- Corrigindo o terceiro código

--- Aula 4-Atividade/editar.php
+++ Aula 4-Atividade/editar.php
@@ -1,22 +1,21 @@
 <?php
 // Edição com erro de lógica (não busca o ID corretamente)
 include("conexao.php");
-
-$id = $_GET["id"];
+ // NÃO ENTENDI
 $sql = "SELECT * FROM usuarios WHERE id = $id";
-$res = mysqli_query($conn, $sql);
+$res = mysqli_query($conn, $sql);   
 $dado = mysqli_fetch_assoc($res);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {  //aspas simples
+    $nome = $_POST['nome'];  ///aspas simples
+    $email = $_POST['email'];
 
     $sql = "UPDATE usuarios SET nome='$nome', email='$email' WHERE id=$id";
     mysqli_query($conn, $sql);
     header("Location: index.php");
 }
 ?>
-
+<!--Não tem o cabeçalho do HTML-->
 <form method="POST">
     Nome: <input type="text" name="nome" value="<?= $dado['nome'] ?>"><br>
     Email: <input type="email" name="email" value="<?= $dado['email'] ?>"><br>
