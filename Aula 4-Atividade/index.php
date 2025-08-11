dcd86f61279f671a95e6cb98cc6a0c30b0751afe
Amanda <Amanda_moller@estudante.sesisenai.og.br>
2025-08-11 15:44:06 -0300

    add- finalizado

--- Aula 4-Atividade/index.php
+++ Aula 4-Atividade/index.php
@@ -5,11 +5,11 @@ include("conexao.php");
 $sql = "SELECT * FORM usuarios"; // Erro de SQL: FORM ao invés de FROM
 $resultado = mysqli_query($conn, $sql);
 
-echo "<h1>Lista de Usuários</h1>";
+echo "<h1>Lista de Usuários</h1>";  // fora de ordem 
 
 while ($linha = mysqli_fetch_array($resultado)) {
     echo "Nome: " . $linha['nome'] . "<br>";
     echo "Email: " . $linha['email'] . "<br><br>";
 }
 ?>
-<a href='cadastrar.php'>Cadastrar novo</a>
+<a href='cadastrar.php'>Cadastrar novo</a>  <!--Ordem incorreta.-->
